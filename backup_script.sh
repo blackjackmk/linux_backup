619cdf868daaf45172c97302655d970ff253d540
Black Jack <kavalchukmaksum@gmail.com>
2025-03-16 16:22:30 +0100

    Starship terminal

--- backup_script.sh
+++ backup_script.sh
@@ -6,6 +6,6 @@ dconf dump / > gnome_settings.dconf
 # restore: dconf load -f / < gnome_settings.dconf
 # terminal custom
 cp ~/.config/starship.toml ~/Documents/starship.toml
-cp ~/.local/share/fonts/Hack/HackNerdFontMono-Regular.ttf ~/Documents/fonts/Hack/HackNerdFontMono-Regular.ttf
+cp ~/.local/share/fonts/Hack/HackNerdFontMono-Regular.ttf ~/Documents/HackNerdFontMono-Regular.ttf
 
 
