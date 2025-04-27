f1b527f936bc89968b3bbbb43aa28ae0bb10a07a
Black Jack <kavalchukmaksum@gmail.com>
2025-04-27 13:42:36 +0200

    before F42 update

--- gnome/backup.sh
+++ gnome/backup.sh
@@ -9,6 +9,9 @@ dconf reset -f /org/gnome/desktop/app-folders/folders/Pardus/
 dconf reset -f /org/gnome/desktop/app-folders/folders/YaST/
 dconf reset -f /org/gnome/evolution-data-server/
 dconf reset -f /com/raggesilver/BlackBox/
+dconf reset -f /org/virt-manager/
+dconf reset -f /org/gnome/portal/filechooser/
+dconf reset -f /org/gnome/desktop/notifications/application/
 #backup main settings
 dconf dump / > ./gnome_settings.dconf
 
